91033a68d4ec04cb17651d60b0483dc56b944c5d
revuriii99 <reviwilerii@gmail.com>
2026-05-25 00:13:47 +0700

    bug fixing

---
+++ Settings/main.js
@@ -0,0 +1,24 @@
+const btnEdit = document.getElementById('btn-edit');
+if (btnEdit) {
+  btnEdit.addEventListener('click', function() {
+    window.location.href = 'layarDua.html';
+  });
+}
+
+const btnBack = document.getElementById('btn-back');
+if (btnBack) {
+  btnBack.addEventListener('click', function() {
+    window.location.href = 'layarSatu.html';
+  });
+}
+
+const navItems = document.querySelectorAll('#bottom-nav div');
+navItems.forEach(function(item) {
+  item.addEventListener('click', function() {
+    navItems.forEach(function(el) {
+      el.classList.remove('active');
+    });
+    this.classList.add('active');
+  });
+});
+
